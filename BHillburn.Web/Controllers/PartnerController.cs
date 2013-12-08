9440c566c931222e80ac5164137c5328bec405da
jesseeuton <jesseeuton@gmail.com>
2013-12-07 17:42:13 -0700

    more changes

---
+++ BHillburn.Web/Controllers/PartnerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BHilburn.Web.Controllers
+{
+    public class PartnerController : Controller
+    {
+        //
+        // GET: /Partner/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ BHillburn.Web/Controllers/ServicesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BHilburn.Web.Controllers
+{
+    public class ServicesController : Controller
+    {
+        //
+        // GET: /Clients/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
