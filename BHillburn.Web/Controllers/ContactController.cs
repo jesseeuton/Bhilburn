3e4512e5e778792e20274fedb401d6b6f1130275
jesseeuton <jesseeuton@gmail.com>
2014-04-21 14:26:34 -0600

    seo optimization\cleanup

--- BHillburn.Web/Controllers/ContactController.cs
+++ BHillburn.Web/Controllers/ContactController.cs
@@ -1,14 +1,14 @@
 ﻿
+using System.Net;
 using System.Net.Mail;
 using System.Text;
+using System.Web.Mail;
+using MailMessage = System.Net.Mail.MailMessage;
 
 namespace BHilburn.Web.Controllers
 {
 
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Web;
     using System.Web.Mvc;
 
     using BHilburn.Web.Models.Contact;
@@ -16,58 +16,47 @@ namespace BHilburn.Web.Controllers
 
     public class ContactController : Controller
     {
-        //
-        // GET: /Contact/
-        [HttpPost, HttpGet]
-        public ActionResult Index(ContactViewModel viewModel)
+        public ActionResult Index()
         {
-            if (viewModel == null ||
-                (string.IsNullOrEmpty(viewModel.Name)
-                 && string.IsNullOrEmpty(viewModel.Info)
-                 && string.IsNullOrEmpty(viewModel.Email)
-                 && string.IsNullOrEmpty(viewModel.Phone)))
-            {
-                viewModel = new ContactViewModel();
-                return View(viewModel);   
-            }
-            else
-            {
-                SubmitContactInfo(viewModel);
-                return RedirectToAction("Index", "Home");
-            }
+            return View(new ContactViewModel());      
         }
 
-        private void SubmitContactInfo(ContactViewModel model)
+        // GET: /Contact/
+        [HttpPost]
+        public ActionResult EmailBri(ContactViewModel submittedContactViewModel)
         {
             try
             {
                 var message = new MailMessage();
-                message.From = new MailAddress(model.Email.Trim());
-                
+                message.From = new MailAddress("jesseeuton@gmail.com");
+
                 var toAddy1 = new MailAddress("bri.euton@jhilburnpartner.com");
                 var toAddy2 = new MailAddress("jesseeuton@gmail.com");
+                
                 message.To.Add(toAddy1);
                 message.To.Add(toAddy2);
 
                 message.Subject = "Inquiry from BriEuton.com";
                 var builder = new StringBuilder();
-                builder.AppendLine(string.Format("Name: {0}", model.Name));
-                builder.AppendLine(string.Format("Email: {0}", model.Email));
-                builder.AppendLine(string.Format("Phone: {0}", model.Phone));
-                builder.AppendLine(string.Format("Info: {0}", model.Info));
+                builder.AppendLine(string.Format("Name: {0}", submittedContactViewModel.Name));
+                builder.AppendLine(string.Format("Email: {0}", submittedContactViewModel.Email));
+                builder.AppendLine(string.Format("Phone: {0}", submittedContactViewModel.Phone));
+                builder.AppendLine(string.Format("Info: {0}", submittedContactViewModel.Info));
                 message.Body = builder.ToString();
 
-                var client = new SmtpClient("smtp.gmail.com", 587);           
+                
+                var client = new SmtpClient("smtp.gmail.com", 587);
+                client.Credentials = new NetworkCredential("jesseeuton@gmail.com", "G81238045*");
                 client.EnableSsl = true;
-               
+
                 client.Send(message);
             }
             catch (Exception ex)
             {
-                
+
             }
 
+            return RedirectToAction("Index", "Home");
         }
-
     }
 }
